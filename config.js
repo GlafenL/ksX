53e924e3f28e73acb89d6c81b2a4c2fdf8091fa0
GlafenL <65963098+GlafenL@users.noreply.github.com>
2020-12-21 19:11:32 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,33 @@
+const token = "";
+const prefix = "";
+const color = ""; // You can put RANDOM, https://discord.js.org/#/docs/main/stable/typedef/ColorResolvable, https://htmlcolorcodes.com/
+const image = ""; // Not required
+const twitch = ""; // You can remove but stream and multi-stream commands will not work
+const multistatus = ["", ""]; // write the status in array : ["first", "second", "third"]
+const rpc = "" // on or off (if you want a rich presence or not)
+const claimer = ""; // on or off
+const premiumkey = ""; // Your premium key here
+const abusedlogs = "" // on or off (show deleted/edited messages in all channels in all servers)
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    token,
+    prefix,
+    color,
+    image,
+    twitch,
+    multistatus,
+    rpc,
+    claimer,
+    premiumkey,
+    abusedlogs
+}
